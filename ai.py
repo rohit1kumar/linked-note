ad01e13608d8b777357577b7f8aae4c0c519ed32
Rohit Kumar <kumar1rohit@outlook.com>
2024-11-07 12:55:49 +0530

    add ai message generator

---
+++ ai.py
@@ -0,0 +1,44 @@
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+from log import logger
+
+load_dotenv()
+
+
+class AI:
+    def __init__(self):
+        self.ai_client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+    def _get_formated_posts(self, profile_data):
+        formatted_posts = ""
+        posts = profile_data["posts"][:5]
+        for i in range(len(posts)):
+            post = posts[i]
+            formatted_posts += "Post " + str(i + 1) + ": " + post + "\n"
+        return formatted_posts.strip()
+
+    def _get_prompt(self, profile_data):
+        formated_posts = self._get_formated_posts(profile_data)
+        return f"""
+        Create a 2-line professional, easy to understand personalized LinkedIn connection request message based on following profile data:
+
+        Name: {profile_data['name']}
+        Headline: {profile_data['headline']}
+        Recent posts: {formated_posts}
+        """
+
+    def ai_response(self, prompt):
+        response = self.ai_client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[{"role": "user", "content": prompt}],
+        )
+        return response.choices[0].message.content
+
+    def generate_connection_message(self, profile_data):
+        try:
+            prompt = self._get_prompt(profile_data)
+            return self.ai_response(prompt)
+        except Exception as e:
+            logger.error(f"Error generating message: {str(e)}")
+            return "Failed to generate connection message"
